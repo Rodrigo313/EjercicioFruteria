8d3f7b08fab58a49d5575dac26735aa7fc335200
Rodrigo Pascual Montero <rodrigopascual1@hotmail.com>
2020-10-30 17:46:11 +0100

    He creado las clases, las instancias y la función para almacenar los kilos

--- fruteria.js
+++ fruteria.js
@@ -1,18 +1,50 @@
-var cantidad = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var kilos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var frutitas = ['manzanas', 'plátanos', 'naranjas', 'nectarinas', 'pinia', 'peritas', 'arándanos', 'kiwi', 'sandia', 'granada'];
-var precioKg = [2.5, 2.0, 1.43, 1.73, 3.59, 1.79, 4.48, 3.85, 0.53, 0.56];
-function anadirCompra(frutas){
-    var i;
-    for(i = 0; i < frutitas.length;i++){
-        if(frutitas[i] == frutas){
-            cantidad[i]+=1;
-            kilos[i]+=1;
-            break;
-        }
+class Frutas{
+    constructor(kilos, precio, nombre){
+        this.kilos = kilos;
+        this.precio = precio;
+        this.nombre = nombre;
+    }
+    getNombre(){
+        return "nombre" + "" + this.nombre;
     }
 }
-function boton(){
+
+class FrutasVerano extends Frutas{
+    constructor(kilos, precio, nombre, proximidad, region){
+        super(kilos, precio, nombre);
+        this.proximidad = proximidad;
+        this.region = region;
+    }
+}
+
+class FrutasInvierno extends Frutas{
+    constructor(kilos, precio, nombre, conservar){
+        super(kilos, precio, nombre);
+        this.conservar = conservar;
+    }
+}
+
+let manzana = new FrutasInvierno(0,2.5,"manzana","no conservar");
+let peras = new FrutasVerano(0,1.79,"peras","de proximidad","Salamanca");
+let platanos = new FrutasVerano(0,2.00,"platanos","de proximidad", "Canarias");
+let arandanos = new FrutasVerano(0,4.48,"arandanos","no de proximidad","Cantabria");
+let naranjas = new FrutasInvierno(0,1.43,"naranjas","no conservar");
+let kiwi = new FrutasInvierno(0,3.85,"kiwi","no conservar");
+let nectarina = new FrutasVerano(0,1.73,"nectarinas","de proximidad","Extremadura");
+let sandia = new FrutasVerano(0,0.53,"sandia","de proximidad","Andalucía");
+let piña = new FrutasInvierno(0,3.59,"pina","conservar");
+let granada = new FrutasInvierno(0,0.56,"granada","conservar");
+
+
+
+let kilosTotal = 0;
+function anadirCompra(){
+    let kilos = parseInt(prompt("¿Cuántos kilos desea comprar?"));
+    kilosTotal = kilos + kilosTotal;
+    console.log(kilosTotal);
+}
+
+/*function boton(){
     document.getElementById("cajaTexto").value="";
     var precioMedio = 0;
     var precioTotal = 0;
@@ -28,4 +60,4 @@ function boton(){
     precioMedio/=contador;
     document.getElementById("cajaTexto").value+="Precio total: " + precioTotal + "\n";
     document.getElementById("cajaTexto").value+="Precio medio: " + precioMedio;
-}
+}*/
